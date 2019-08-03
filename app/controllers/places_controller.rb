167e1bbb9a15a60843155c6480d64917003e8d01
Jason McBride <jmcbride.isye@gmail.com>
2019-08-03 21:26:40 +0000

    securing db uploads/etc

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  # this confirms that user is signed in in order to see the page
+  before_action :authenticate_user!, only: [:new,:create, :edit, :update]
 
   def index
     @places = Place.order("name").page(params[:page]).per_page(3)
@@ -20,10 +21,19 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+  
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
